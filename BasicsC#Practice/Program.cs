7a828960b0fa84c5b4f431fc8c2703559b22d220
jaffar806 <146517898+jaffar806@users.noreply.github.com>
2024-09-12 09:28:54 +0530

    Added one more book

--- BasicsC#Practice/Program.cs
+++ BasicsC#Practice/Program.cs
@@ -20,12 +20,18 @@ namespace BasicsC_Practice
                 Title = "Clean code",
                 Author ="jafar"
             };
+            Book book3 = new Book
+            {
+                Title = "Git",
+                Author = "Ramesh"
+            };
 
             bookList.Add(book1);
             bookList.Add(book);
-
+            bookList.Add(book3);
             Console.WriteLine(bookList[0].Title+"," +bookList[0].Author);
             Console.WriteLine(bookList[1].Title+"," +bookList[1].Author);
+            Console.WriteLine(bookList[2].Title+"," + bookList[2].Author);
         }
      
     }
