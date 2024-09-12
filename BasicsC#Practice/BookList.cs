5d9ffd395da68c820832bb8fad47a6d94da25d90
jaffar806 <146517898+jaffar806@users.noreply.github.com>
2024-09-12 09:23:49 +0530

    Add project files.

---
+++ BasicsC#Practice/BookList.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicsC_Practice
+{
+    public class BookList
+    {
+        public List<Book> books = new List<Book>();
+       
+        public void Add(Book book)
+        {
+            books.Add(book);
+        }
+        public Book this[int index]{get{return books[index];}}
+
+    }
+}
---
+++ BasicsC#Practice/Program.cs
@@ -0,0 +1,48 @@
+﻿using System.Linq;
+using System.Runtime.Intrinsics.Arm;
+
+
+
+namespace BasicsC_Practice
+{
+    public class Program
+    {
+        static void Main()
+        {
+            BookList bookList = new BookList();
+            Book book = new Book
+            {
+                Title = "c# in Depth",
+                Author = "Mosh"
+            };
+            Book book1 = new Book
+            {
+                Title = "Clean code",
+                Author ="jafar"
+            };
+
+            bookList.Add(book1);
+            bookList.Add(book);
+
+            Console.WriteLine(bookList[0].Title+"," +bookList[0].Author);
+            Console.WriteLine(bookList[1].Title+"," +bookList[1].Author);
+        }
+     
+    }
+   
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
